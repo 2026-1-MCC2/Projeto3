09ffa5050452688dc2b5fbced5f2e2049d90121f
William Takeuchi Takaki <william.takeuchi@gmail.com>
2026-04-24 11:28:47 -0300

    Adicionando mais coisas ao HTML e correção de bugs.
    Criação tela inicial do site, cores, botões e correção de bugs.
    
    Co-Authored-By: Arthur Henrique dos Anjos Ferreira <arthur.henrique.anjos04@gmail.com>

---
+++ DEV_Site_Mr.Nuts/Frontend/Js/app.js
@@ -0,0 +1,12 @@
+function irParaMarketplace() {
+    alert("Indo para o Marketplace");
+  }
+  
+  function cadastrarFornecedor() {
+    alert("Abrir cadastro de fornecedor");
+  }
+  
+  function entrar() {
+    alert("Abrir tela de login");
+  }
+  
